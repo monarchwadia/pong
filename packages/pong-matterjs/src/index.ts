c4a59d60e31c22b2f3c401da3caddc7d9187740f
Monarch Wadia <monarch@mintbean.io>
2021-04-04 00:21:32 -0400

    Added basic physics

--- packages/pong-matterjs/src/index.ts
+++ packages/pong-matterjs/src/index.ts
@@ -1,17 +1,44 @@
 import Matter from "matter-js";
-const { Engine, Render, World, Bodies } = Matter;
+const { Engine, Render, World, Bodies, Runner } = Matter;
+
+const WIDTH = 800;
+const HEIGHT = 600;
+const WALL_THICKNESS = 20;
 
 const engine = Engine.create();
-const render = Render.create({
-  element: document.body,
-  engine,
-});
+engine.world.gravity.y = 0;
 
 const boxA = Bodies.rectangle(400, 200, 80, 80);
 const boxB = Bodies.rectangle(450, 50, 80, 80);
-const ground = Bodies.rectangle(400, 610, 810, 60, { isStatic: true });
 
-World.add(engine.world, [boxA, boxB, ground]);
+const wall = (x: number, y: number, width: number, height: number) =>
+  Bodies.rectangle(x, y, width, height, {
+    isStatic: true,
+    render: {
+      fillStyle: "#868e96",
+    },
+  });
 
-Engine.run(engine);
+const walls = [
+  wall(WIDTH / 2, 0, WIDTH, WALL_THICKNESS), // top
+  wall(WIDTH / 2, HEIGHT, WIDTH, WALL_THICKNESS), // bottom
+  wall(0, HEIGHT / 2, WALL_THICKNESS, HEIGHT), // left
+  wall(WIDTH, HEIGHT / 2, WALL_THICKNESS, HEIGHT), // right
+];
+
+World.add(engine.world, [boxA, boxB, ...walls]);
+
+const runner = Runner.create();
+Runner.run(runner, engine);
+
+const render = Render.create({
+  element: document.body,
+  engine,
+  options: {
+    width: WIDTH,
+    height: HEIGHT,
+    background: "#000000",
+    wireframes: false,
+  },
+});
 Render.run(render);
